9f7c8c25158c41f8581beab453d03acc6cd6679b
0x1Jar <fajar3842@gmail.com>
2025-06-20 23:25:13 +0700

    add project

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 //import module
 mod hello_rust;
+mod project {
+    pub mod gues_progame;
+    pub mod generate_rondom;
+}
 
 fn main() {
     println!("Hello, world!");
 
     //this is comment
+    //run progame
     println!("i'm Rust 0x1jar");
 
     //run progame hello_rust
     let run: &'static str = hello_rust::hello_rust();
 
     println!("{:?}",run);
+
+    //run progame gues_progame
+    //let run1: () = 
+    project::gues_progame::run_progame();
+
+    // println!("{:?}", run1);
+
+    project::generate_rondom::run_rondom();
 
 }
 
