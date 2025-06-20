957ea9006659f80ece1e9739ad55f1c1f1329cfe
0x1Jar <fajar3842@gmail.com>
2025-06-20 21:58:45 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+mod hello_rust;
+
 fn main() {
     println!("Hello, world!");
-    println
+
+    //this is comment
+    println!("i'm Rust 0x1jar");
+
+    //run progame hello_rust
+    let run: &'static str = hello_rust::hello_rust();
+
+    println!("{:?}",run);
+    
 }
+
+
